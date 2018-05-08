6dfdbc6b266de5427a1bab3531fc870107c51063
Dennis Moyseyev <denomoyseyev@gmail.com>
2018-05-07 22:10:07 -0500

    First commit

---
+++ app/src/main/java/com/example/dennismoyseyev/stockinfo/Ticker_input.kt
@@ -0,0 +1,12 @@
+package com.example.dennismoyseyev.stockinfo
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class Ticker_input : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ticker_input)
+    }
+}
